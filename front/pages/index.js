e6548699e4dc324dd556c0c3eb3d017631b15ccf
leejungwu <ljw538@naver.com>
2020-12-08 22:17:36 +0900

    Update: cookie samesite:none

--- front/pages/index.js
+++ front/pages/index.js
@@ -51,7 +51,7 @@ export const getServerSideProps = wrapper.getServerSideProps(async (context) => 
   console.log('getServerSideProps start');
   console.log(context.req.headers);
   const cookie = context.req ? context.req.headers.cookie : '';
-  axios.defaults.headers.Cookie = '';
+  axios.defaults.headers.Cookie = 'None';
   if (context.req && cookie) {
     axios.defaults.headers.Cookie = cookie;
   }
