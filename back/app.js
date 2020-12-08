015b3269c374741ae594073ba38bc5680bf68c01
leejungwu <ljw538@naver.com>
2020-12-08 23:10:54 +0900

    Fix: cors

--- back/app.js
+++ back/app.js
@@ -30,14 +30,18 @@ if (process.env.NODE_ENV === 'production') {
   app.use(morgan('combined'));
   app.use(hpp());
   app.use(helmet.referrerPolicy({ policy: 'strict-origin-when-cross-origin' }))
+  app.use(cors({
+    origin: 'http://dcutime.shop',
+    credentials: true,
+  }));
 } else {
   app.use(morgan('dev'));
+  app.use(cors({
+    origin: true,
+    credentials: true,
+  }));
 }
 
-app.use(cors({
-  origin: ['http://localhost:3060','dcutime.shop'],
-  credentials: true,
-}));
 app.use('/', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
