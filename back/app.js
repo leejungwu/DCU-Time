376614f2f0a9952d3626feebe936f829b27552a4
leejungwu <ljw538@naver.com>
2020-12-09 16:29:08 +0900

    Update

--- back/app.js
+++ back/app.js
@@ -26,6 +26,7 @@ db.sequelize.sync()
   .catch(console.error);
 passportConfig();
 
+app.set('trust proxy', 1);
 if (process.env.NODE_ENV === 'production') {
   app.use(morgan('combined'));
   app.use(hpp());
@@ -50,6 +51,7 @@ app.use(session({
   saveUninitialized: false,
   resave: false,
   secret: process.env.COOKIE_SECRET,
+  proxy: true,
   cookie: {
     httpOnly: true,
     secure: false,
